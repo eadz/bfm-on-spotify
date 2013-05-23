7e3bf0936f10e667b34d339fc5f58be6a9b48a76
Eaden McKee <eadz@eadz.co.nz>
2013-05-23 14:48:23 +1200

    done

--- _code/generator.rb
+++ _code/generator.rb
@@ -2,22 +2,63 @@ require 'rubygems'
 require 'nokogiri'
 require 'open-uri'
 require 'json'
+require 'active_record'
+require 'sqlite3'
+require 'logger'
+
+ActiveRecord::Base.establish_connection(:adapter => "sqlite3", :database => "_code/songs.sqlite3")
+ActiveRecord::Base.logger = Logger.new(STDOUT)
+
+# ActiveRecord::Migration.create_table :songs do |t|
+#   t.string :song_name
+#   t.string :spotify_id
+#   t.timestamps
+# end
+
+class Song < ActiveRecord::Base
+end
 
 def get_songs_from_djpage(djpage)
   songs = djpage.at_css("ul.djextra").children.map{|c| c.text.gsub(/\n|\t/,' ').gsub(/ {1,99}/,' ').strip}.compact
   songs.delete_if{|s| s == ""}
-  songs
+  songs.map!{|s| s.split("[").first }
+  i = 1
+  while(i < songs.length)
+    if songs[i][0].ord == 8211 # Zac and Ethan.., use two lines for their playlist.
+      puts "Got it!! #{songs[i-1]} and #{songs[i]}"
+      songs[i-1] += songs[i] rescue nil
+      songs[i] = nil
+    end
+    i+=1
+  end
+  songs.compact
 end
 
 def get_spotify_url_from_song(song)
-  url = "http://ws.spotify.com/search/1/track.json?q=#{URI::encode(song)}"
-  begin
-    res = JSON.parse(open(url).read)
-    if res["tracks"] && r = res["tracks"].first
-      r["href"]
+  s = Song.where(:song_name => song)
+  if songrecord = s.first
+    if songrecord.spotify_id == 'notfound'
+      return nil
+    else
+      return songrecord.spotify_id
     end
-  rescue Exception => e
-    puts e
+  else
+    url = "http://ws.spotify.com/search/1/track.json?q=#{URI::encode(song)}"
+    begin
+      res = JSON.parse(open(url).read)
+      songrecord = s.new
+      if res["tracks"] && r = res["tracks"].first
+        songrecord.spotify_id = r["href"].split(":").last
+        songrecord.save!
+        return songrecord.spotify_id
+      else
+        songrecord.spotify_id = 'notfound'
+        songrecord.save!
+        return nil
+      end
+    rescue Exception => e
+      puts e.inspect
+    end
   end
 end
 
@@ -25,13 +66,11 @@ def get_playlist_url(djpage)
   songs = get_songs_from_djpage(djpage)
   playlist_url = "https://embed.spotify.com/?uri=spotify:trackset:bFMonSpotify:"
 
-  while(playlist_url.length < 1000 && songs.length > 0)
+  while(playlist_url.length < 1700 && songs.length > 0)
     s = songs.shift
-    sleep 0.1
     url = get_spotify_url_from_song(s)
     if url
-      puts url.split(":").last
-      playlist_url += url.split(":").last + ","
+      playlist_url += url + ","
     end
   end
   playlist_url
@@ -51,9 +90,9 @@ File.open("_includes/playlists.html", "w") do |f|
     begin
       djpage = Nokogiri::HTML(open(url))
       playlist_url = get_playlist_url(djpage)
-      f.write("<li><a target=\"_blank\" href=\"#{playlist_url}\">#{d[0]}</a></li>\n")
+      f.write("<li><a target=\"_blank\" href=\"#{playlist_url}\">#{d[0]}</a> (<a href=\"#{url}\">@bfm</a>)</li>\n")
     rescue Exception => e
-      puts "Error with DJ page #{d[0]} - #{d[1]}"
+      puts "Error with DJ page #{d[0]} - #{d[1]} #{e.inspect}"
     end
     f.flush # 
   end
